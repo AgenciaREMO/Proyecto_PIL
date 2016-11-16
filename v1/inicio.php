70622463e3ae6f86bf1b2b20741d3be07f018beb
Angie <ma.angeles.remo@gmail.com>
2016-11-15 18:49:52 -0600

    Inicio
    FALTA QUITAR EL MARGEN DE ABAJO DEL CHEF QUE DA LATA

--- v1/inicio.php
+++ v1/inicio.php
@@ -19,16 +19,73 @@
 		menu("1");
 	?>
 	<div class="bg-lanzamiento">
+		<header id="carousel-inicio" class="carousel slide">
+	        <!-- Indicators -->
+	        <ol class="carousel-indicators">
+	            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	            <li data-target="#myCarousel" data-slide-to="1"></li>
+	            <li data-target="#myCarousel" data-slide-to="2"></li>
+	            <li data-target="#myCarousel" data-slide-to="3"></li>
+	        </ol>
+
+	        <!-- Wrapper for slides -->
+	        <div class="rojo">
+	        </div>
+	        <div class="carousel-inner">
+	            <div class="item active">
+	                <a href="">
+	                	<div class="fill" >
+	                		<img class="img-responsive slide-inicio" src="rec/video/animacion-pechuga-navidena.mp4" >
+		                </div>
+	                </a>
+	                <div class="carousel-caption">
+	                </div>
+	            </div>
+	            <div class="item">
+	                <div class="fill" >
+	                		<img class="img-responsive slide-inicio" src="rec/video/animacion-nuggets.mp4" >
+	                </div>
+
+	                <div class="carousel-caption">
+	                </div>
+	            </div>
+	            <div class="item">
+	                <div class="fill" >
+	                		<img class="img-responsive slide-inicio" src="rec/video/animacion-boneless.mp4" >
+	                </div>
+	                <div class="carousel-caption">
+	                </div>
+	            </div>
+	            <div class="item">
+	                <div class="fill" >
+	                		<img class="img-responsive slide-inicio" src="rec/video/animacion-arrachera.mp4" >
+	                </div>
+	                <div class="carousel-caption">
+	                </div>
+	            </div>
+	        </div>
+	        <div class="rojo">
+	        </div>
+	        <!-- Controls -->
+	        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+	            <span class="icon-prev"></span>
+	        </a>
+	        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+	            <span class="icon-next"></span>
+	        </a>
+	    </header>
+
+
+
 	<!--<div class="app-container" data-bb="app-main-region">-->
-		<div class="hero main">
+		<!--<div class="hero main">
 		  <div class="embed-responsive embed-responsive-16by9 vid-bg hidden-sm hidden-xs">
 		    <video autoplay="" loop="" muted="" class="embed-responsive-item" controls>
 		    	<source src="rec/video/nescafe-background.mp4" type="video/mp4">
 		    </video>
-		    <!--<button class="mute on"></button>-->
 		  </div>
 		  <img src="rec/img/16x9.png" class="ratio spacer hidden-sm hidden-xs">
-		  <img src="//prod1.nescafe.com/es_es/public/img/hero/landing-mobile.gif" class="mobile-hero hidden-md hidden-lg hidden-xl"> <!-- Video Movil-->
+		  <img src="//prod1.nescafe.com/es_es/public/img/hero/landing-mobile.gif" class="mobile-hero hidden-md hidden-lg hidden-xl"> 
 		  <div id="cont-video" class="vertical-center">
 		    <div class="vertical-center-inner">
 		      <div class="hero-content">
@@ -39,14 +96,14 @@
 		          <h4>
 		            Somos el segundo mayor productor de pollo en el mundo. A lo largo de 70 años, en Pilgrim’s, hemos producido alimentos con altos estándares de calidad. Estamos comprometidos con los más de 35,000 miembros del equipo que trabajan con nosotros para ofrecer productos a mayoristas, minoristas, instituciones, restaurantes, autoservicios y clientes.
 		          </h4>
-		        </div><!-- /.hero-cta -->
+		        </div>
 		         <div class="hero-head">
 		          <img src="rec/img/linea-video.png" class="img-responsive center-block">
 		        </div>
-		      </div><!-- /.hero-content -->
-		    </div><!-- /.vertical-center-inner -->
+		      </div>
+		    </div>
 		  </div>
-		</div>
+		</div>-->
 		
 		  	<div class="gris-superior">
         	</div>
@@ -59,7 +116,7 @@
 			</div>
 		</div>
 	</section>
-    <header id="myCarousel" class="carousel slide">
+    <header id="carousel-lanzamientos" class="carousel slide">
         <!-- Indicators -->
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -143,6 +200,8 @@
 		<img class="img-responsive" src="rec/img/promocion.jpg">
 		<div class="gris-inferior"></div>
 	</div>
+
+    <!--
 	<section class="container text-center ">
 		<div class="row plecas">
 			<div class="col-lg-12 col-md-12 col-sm-12 ">
@@ -184,10 +243,128 @@
 	        </div>
 	    </div>
 	    <br>
-	</div>
+	</div>-->
 	</div>
     <div class="bg-inicio">
     	<div class="container">
+    		<div id="esc-rec-ini" class="row">
+    			<div >
+    				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    					<img  id="chef" class="img-responsive" src="rec/img/chef-omar.png" >
+    				</div>
+    				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+    					<img id="recetas" class="img-responsive" src="rec/img/recetas.png" >
+    				</div>
+    				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+    					<div class="bg-recetas">
+				    			<div class="row">
+					    			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 recetas-inicio">
+									    <div class="hovereffect">
+									        <img class="img-responsive" src="rec/img/recetas/espagueti_de_pollo_en_salsa_de_tomate.jpg">
+									            <div class="overlay">
+									            	<hr class="hr-hover">
+									                <h2>Nombre de la receta</h2>
+													<p> 
+														<a href="#" >
+															Ver receta
+														</a>
+													</p>
+													<hr class="hr-hover"> 
+									            </div>
+									    </div>
+									</div>
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 recetas-inicio">
+									    <div class="hovereffect">
+									        <img class="img-responsive" src="rec/img/recetas/medallones_de_pollo_estilo_Colima.jpg">
+									            <div class="overlay">
+									            	<hr class="hr-hover">
+									                <h2>Nombre de la receta</h2>
+													<p> 
+														<a href="#">
+															Ver receta
+														</a>
+													</p>
+													<hr class="hr-hover">
+									            </div>
+									    </div>
+									</div>
+					    		</div>
+				    			<div class="row">
+					    			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 recetas-inicio">
+									    <div class="hovereffect">
+									        <img class="img-responsive " src="rec/img/recetas/medallones_de_pollo_con_brocoli_en_salsa_de_queso.jpg">
+									            <div class="overlay">
+									            	<hr class="hr-hover">
+									                <h2>Nombre de la receta</h2>
+													<p> 
+														<a href="#" >
+															Ver receta
+														</a>
+													</p>
+													<hr class="hr-hover"> 
+									            </div>
+									    </div>
+									</div>
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 recetas-inicio">
+									    <div class="hovereffect">
+									        <img class="img-responsive" src="rec/img/recetas/alitas_de_pollo_en_salsa_de_tamarindo_picante_sobre_aranitas_de_plato.jpg">
+									            <div class="overlay">
+									            	<hr class="hr-hover">
+									                <h2>Nombre de la receta</h2>
+													<p> 
+														<a href="#">
+															Ver receta
+														</a>
+													</p>
+													<hr class="hr-hover">
+									            </div>
+									    </div>
+									</div>
+				    			</div>
+				    		</div>
+    				</div>
+    			</div>
+    		</div>
+    		<section id="mob-re-ini" >
+    				<div class="row">
+    					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    						<img style="margin:auto !important;" id="recetas" class="img-responsive" src="rec/img/recetas.png" >
+    					</div>
+    				</div>
+    					<div class="bg-recetas">
+    							<br>
+    							<div class="row">
+					    			<div class="col-lg-2 col-md-2 col-sm-2  col-xs-4">
+					    				<img class="img-responsive img-thumbnail" src="rec/img/recetas/espagueti_de_pollo_en_salsa_de_tomate.jpg">
+					    			</div>
+					    			<div class="col-lg-10 col-md-10 col-sm-10">
+					    				<a href="receta_uno.php"><p>ALITAS DE POLLO CON TRIPLE COMPAÑIA</p></a>
+					    			</div>
+					    		</div>
+					    		<br>
+					    		<div class="row">
+					    			<div class="col-lg-2 col-md-2 col-sm-2  col-xs-4">
+					    				<img class="img-responsive img-thumbnail" src="rec/img/recetas/espagueti_de_pollo_en_salsa_de_tomate.jpg">
+					    			</div>
+					    			<div class="col-lg-10 col-md-10 col-sm-10">
+					    				<a href="receta_uno.php"><p>PECHUGA GLASEADA AL CHABACANO CON VEGETALES</p></a>
+					    			</div>
+					    		</div>
+					    		<br>
+					    		<div class="row">
+					    			<div class="col-lg-2 col-md-2 col-sm-2  col-xs-4">
+					    				<img class="img-responsive img-thumbnail" src="rec/img/recetas/espagueti_de_pollo_en_salsa_de_tomate.jpg">
+					    			</div>
+					    			<div class="col-lg-10 col-md-10 col-sm-10">
+					    				<a href="receta_uno.php"><p>POLLO ALMENDRADO</p></a>
+					    			</div>
+					    		</div>
+					    		<br>
+				    	</div>
+    		</section>
+
+
+<!--
     		<section class="esc-re-ini">
     			<table width="100%">
     				<tr>
@@ -320,71 +497,7 @@
     					</td>
     				</tr>
     			</table>
-    		</section>
-    		<section class="mob-re-ini">
-    				<div class="row">
-    					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    						<img style="margin:auto !important;" id="recetas" class="img-responsive" src="rec/img/recetas.png" >
-    					</div>
-    				</div>
-    					<div class="bg-recetas">
-    							<br>
-    							<div class="row">
-					    			<div class="col-lg-2 col-md-2 col-sm-2  col-xs-4">
-					    				<img class="img-responsive img-thumbnail" src="rec/img/recetas/espagueti_de_pollo_en_salsa_de_tomate.jpg">
-					    			</div>
-					    			<div class="col-lg-10 col-md-10 col-sm-10">
-					    				<a href="receta_uno.php"><p>RECETA UNO</p></a>
-					    			</div>
-					    		</div>
-					    		<br>
-					    		<div class="row">
-					    			<div class="col-lg-2 col-md-2 col-sm-2  col-xs-4">
-					    				<img class="img-responsive img-thumbnail" src="rec/img/recetas/espagueti_de_pollo_en_salsa_de_tomate.jpg">
-					    			</div>
-					    			<div class="col-lg-10 col-md-10 col-sm-10">
-					    				<a href="receta_uno.php"><p>RECETA DOS</p></a>
-					    			</div>
-					    		</div>
-					    		<br>
-					    		<div class="row">
-					    			<div class="col-lg-2 col-md-2 col-sm-2  col-xs-4">
-					    				<img class="img-responsive img-thumbnail" src="rec/img/recetas/espagueti_de_pollo_en_salsa_de_tomate.jpg">
-					    			</div>
-					    			<div class="col-lg-10 col-md-10 col-sm-10">
-					    				<a href="receta_uno.php"><p>RECETA TRES</p></a>
-					    			</div>
-					    		</div>
-					    		<br>
-					    		<div class="row">
-					    			<div class="col-lg-2 col-md-2 col-sm-2  col-xs-4">
-					    				<img class="img-responsive img-thumbnail" src="rec/img/recetas/espagueti_de_pollo_en_salsa_de_tomate.jpg">
-					    			</div>
-					    			<div class="col-lg-10 col-md-10 col-sm-10">
-					    				<a href="receta_uno.php"><p>RECETA CUATRO</p></a>
-					    			</div>
-					    		</div>
-					    		<br>
-				    	</div>
-    		</section>
-    		<!--<div class="bg-recetas row">
-	    		<div class="col-lg-4 col-md-6 col-sm-12">
-	    			<img class="img-responsive" src="rec/img/chilles-rellenos.jpg">
-	    			<video  width="100%" controls">
-			    		<source src="rec/video/nescafe-background.mp4" type="video/mp4">
-			    	</video>
-	    		</div>
-	    		<div class="col-lg-4 col-md-6 col-sm-12">
-	    			<img class="img-responsive" src="rec/img/nopales-pollo.jpg">
-	    			<video  width="100%" controls">
-			    		<source src="rec/video/nescafe-background.mp4" type="video/mp4">
-			    	</video>
-	    		</div>
-	    		<div class="col-lg-4 col-md-6 col-sm-12">
-	    			<img class="img-responsive" src="rec/img/pollo-garbanzo-arroz.jpg">
-	    			<video  width="100%" controls">
-			    		<source src="rec/video/nescafe-background.mp4" type="video/mp4">
-			    	</video>-->
+    		</section>-->
 	    		</div>
     		</div>
     	</div>
@@ -396,8 +509,11 @@
 
     <!-- Script to Activate the Carousel -->
     <script>
-    $('.carousel').carousel({
+    $('#carousel-lanzamientos').carousel({
         interval: 5000 //changes the speed
+    })
+    $('#carousel-inicio').carousel({
+        interval: 15000 //changes the speed
     })
     </script>
 
