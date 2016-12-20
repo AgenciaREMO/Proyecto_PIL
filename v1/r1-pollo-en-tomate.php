97f65757a41afdb735e18a58e79b637795f92f23
Angie Godinez <ma.angeles.remo@gmail.com>
2016-12-20 11:11:55 -0600

    Recetas
    Maquetación de las recetas de la versión de escritorio

--- v1/r1-pollo-en-tomate.php
+++ v1/r1-pollo-en-tomate.php
@@ -34,134 +34,203 @@
 			  <li><a href="recetario.php">Recetario</a></li>
 			  <li class="active">Espagueti con pollo Pilgrim’s en salsa de tomate</li>
 			</ol>
-			<div class="cont-recetas">
-				<div class="row esc-recetario">
-					<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-						<p class="title-receta">Espagueti con pollo Pilgrim’s</p>
-						<p class="com-receta">en salsa de tomate</p>
-						<p class="rinde"><b><i>* Rinde para 4 personas<i></b></p>
-					</div>
-					<div class="col-lg-offset-3 col-lg-1 col-md-offset-3 col-md-1 col-sm-12 col-xs-12 incomparable" >
-						<img class="img-responsive logo-incomparable" src="rec/img/incomparable.png">
-					</div>
-					<!--<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 firma">
-						<img class="img-responsive" src="rec/img/firma.png">
-					</div>-->
-				</div>
-				<div class="row mob-recetario">
-					<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-						<p class="title-receta">Espagueti con pollo Pilgrim’s</p>
-						<p class="com-receta">en salsa de tomate</p>
-						<p class="rinde"><b><i>* Rinde para 4 personas<i></b></p>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 incomparable" >
-						<img class="img-responsive logo-incomparable" style="margin: auto;" src="rec/img/incomparable.png">
-					</div>
-					<!--<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 firma">
-						<img class="img-responsive" style="margin: auto;" src="rec/img/firma-mob.png">
-					</div>-->
-				</div>
-				<br>
-				<div class="row">
-					<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 " >
-						<img src="rec/img/gallina.png" class="img-responsive gallina">
-						<div class="ingredientes">
-							Ingredientes
-						</div>
-						<div class="ing">
-							<p>4 Mitades de <a href="milanesas.php" data-toggle="tooltip" data-placement="top" title="Ver producto"><b><i>Pechuga de Pollo Pilgrim’s</i></b></a> sin hueso.</p>
-							<p>2 Cucharadas de aceite de oliva.</p>
-							<p>1/2 Cebolla picada.</p>
-							<p>1 Zanahoria picada.</p>
-							<p>1 Tallo de apio con hojas, lavado y sin fibra, picado.</p>
-							<p>2 Diente de ajo machacado.</p>
-							<p>1/2 Taza de vino blanco seco.</p>
-							<p>1 Lata de puré de tomate.</p>
-							<p>1/2 Taza de caldo de pollo.</p>
-							<p>1 Cucharada de perejil lavado y picado.</p>
-							<p>3 Cucharadas de albahaca fresca, lavada y picada.</p>
-							<p>Sal y pimienta al gusto.</p>
-							<p>500 Gramos de espagueti.</p>
-						</div>
-					</div>
-					<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 " >
-						<img src="rec/img/cubiertos.png" class="img-responsive gallina">
-						<div class="hr-title-recetas"></div>
-							<div class="procedimiento">
-								Procedimiento
+
+			<section class="esc-recetario">
+				<div class="cont-recetas">
+					<div class="row encabezado">
+						<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+							<div class="titulos">
+								Espagueti con pollo Pilgrim’s<br> en salsa de tomate
 							</div>
-						<div class="hr-title-recetas"></div>
-						<table width="97%">
-							<tr>
-								<td width="13%;" >
-									<img style="margin: auto;" src="rec/img/uno.png" class="img-responsive">
-								</td>
-								<td style="padding: 2%; font-style: normal; color: #6e6e6d; font-size: 1.2em">
-									En una cacerola a fuego medio agregar aceite y calentar. Añadir y acitronar la cebolla, zanahoria y perejil durante 5 minutos
-								</td>
-							</tr>
-							<tr>
-								<td width="13%;" >
-									<img style="margin: auto;" src="rec/img/dos.png" class="img-responsive">
-								</td>
-								<td style="padding: 2%; font-style: normal; color: #6e6e6d; font-size: 1.2em">
-									Añadir el <b><i><a href="milanesas.php" data-toggle="tooltip" data-placement="top" title="Ver producto">pollo Pilgrim’s Premium</a></i></b> y sellar por ambos lados; añadir el vino blanco y dejar reducir a fuego alto por 10 minutos.
-								</td>
-							</tr>
-							<tr>
-								<td width="13%;" >
-									<img style="margin: auto;" src="rec/img/tres.png" class="img-responsive">
-								</td>
-								<td style="padding: 2%; font-style: normal; color: #6e6e6d; font-size: 1.2em">
-									Mezclar el purè de jitomate con el caldo de pollo y el perejil e incorporar al pollo, dejar cocer por 30 minutos hasta que la salsa se espese.
-								</td>
-							</tr>
-							<tr>
-								<td width="13%;" >
-									<img style="margin: auto;" src="rec/img/cuatro.png" class="img-responsive">
-								</td>
-								<td style="padding: 2%; font-style: normal; color: #6e6e6d; font-size: 1.2em">
-									Cocinar la pasta según las instrucciones del empaque, colar y añadir en el pollo y cocinar por 2 minutos y servir.
-								</td>
-							</tr>
-							<tr>
-								<td width="13%;" >
-									<img style="margin: auto;" src="rec/img/cinco.png" class="img-responsive">
-								</td>
-								<td style="padding: 2%; font-style: normal; color: #6e6e6d; font-size: 1.2em">
-									Sazonar con sal y pimienta al gusto y añadir albahaca. 
-								</td>
-							</tr>
-						</table>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+							<img id="rec-inco" src="rec/img/recetas/incomparable.png" class="img-responsive">
+						</div>
+					</div>
+					<div class="hoja">
+						<div class="row">
+							<div class="ingredientes col-lg-6 col-md-6 col-sm-12 col-xs-12">
+								Ingredientes
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+								<p>4 Mitades de <b><i>Pechuga de Pollo Pilgrim’s</i></b> sin hueso.</p><br>
+								<a href="milanesas.php" data-toggle="tooltip" data-placement="top" title="Ver producto">
+									<img id="producto" src="rec/img/recetas/pollo-fresco.png" class="img-responsive">
+								</a> 
+								<br>
+								<p>2 Cucharadas de aceite de oliva.</p>
+								<p>1/2 Cebolla picada.</p>
+								<p>1 Zanahoria picada.</p>
+								<p>1 Tallo de apio con hojas, lavado y sin fibra, picado.</p>
+								<p>2 Diente de ajo machacado.</p>
+							</div>
+							<div class="col-lg-1 col-md-1 hidden-sm-down ">
+								<img src="rec/img/recetas/linea.png" class="img-responsive">
+							</div>
+							<div class="col-lg-6 col-md-6 hidden-xs-down">
+								<p>1/2 Taza de vino blanco seco.</p>
+								<p>1 Lata de puré de tomate.</p>
+								<p>1/2 Taza de caldo de pollo.</p>
+								<p>1 Cucharada de perejil lavado y picado.</p>
+								<p>3 Cucharadas de albahaca fresca, lavada y picada.</p>
+								<p>Sal y pimienta al gusto.</p>
+								<p>500 Gramos de espagueti.</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+									<img src="rec/img/recetas/reloj.png" class="img-responsive iconos">
+									<div class="indicadores">
+										<i><b>Tiempo de preparación: 60 minutos</b></i>
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+									<img src="rec/img/recetas/rinde.png" class="img-responsive iconos">
+									<div class="indicadores">
+										<i><b>Rinde para : 4 personas</b></i>
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+									<img src="rec/img/recetas/tipo.png" class="img-responsive iconos">
+									<div class="indicadores">
+										<i><b>Comida</b></i>
+									</div>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+									<img src="rec/img/recetas/reloj.png" class="img-responsive iconos">
+									<div class="indicadores">
+										<i><b>Descargar</b></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+					<div class="row container">
+						<div class=" col-lg-7 col-md-7 procedimiento  sombra">
+							<div class="ingredientes col-lg-12 col-md-12 col-sm-12 col-xs-12">
+								<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">Procedimiento</div>
+								<table width="97%">
+									<tr>
+										<td width="13%;" >
+											<img style="margin: auto;" src="rec/img/recetas/1.png" class="img-responsive numeros">
+										</td>
+										<td style="font-family:'Cronos-Pro'; padding: 2%; font-style: normal; color: #6e6e6d; font-size: 0.7em">
+											En una cacerola a fuego medio agregar aceite y calentar. Añadir y acitronar la cebolla, zanahoria y perejil durante 5 minutos
+										</td>
+									</tr>
+									<tr>
+										<td width="13%;" >
+											<img style="margin: auto;" src="rec/img/recetas/2.png" class="img-responsive numeros">
+										</td>
+										<td style="font-family:'Cronos-Pro'; padding: 2%; font-style: normal; color: #6e6e6d; font-size: 0.7em">
+											Añadir el pollo Pilgrim’s Premium y sellar por ambos lados; añadir el vino blanco y dejar reducir a fuego alto por 10 minutos.
+										</td>
+									</tr>
+									<tr>
+										<td width="13%;" >
+											<img style="margin: auto;" src="rec/img/recetas/3.png" class="img-responsive numeros">
+										</td>
+										<td style="font-family:'Cronos-Pro'; padding: 2%; font-style: normal; color: #6e6e6d; font-size: 0.7em">
+											Mezclar el purè de jitomate con el caldo de pollo y el perejil e incorporar al pollo, dejar cocer por 30 minutos hasta que la salsa se espese.
+										</td>
+									</tr>
+									<tr>
+										<td width="13%;" >
+											<img style="margin: auto;" src="rec/img/recetas/4.png" class="img-responsive numeros">
+										</td>
+										<td style="font-family:'Cronos-Pro'; padding: 2%; font-style: normal; color: #6e6e6d; font-size: 0.7em">
+											Cocinar la pasta según las instrucciones del empaque, colar y añadir en el pollo y cocinar por 2 minutos y servir.
+										</td>
+									</tr>
+									<tr>
+										<td width="13%;" >
+											<img style="margin: auto;" src="rec/img/recetas/5.png" class="img-responsive numeros">
+										</td>
+										<td style="font-family:'Cronos-Pro'; padding: 2%; font-style: normal; color: #6e6e6d; font-size: 0.7em">
+											Sazonar con sal y pimienta al gusto y añadir albahaca. 
+										</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+						<div class="col-lg-5 col-md-5 hidden-sm-down hidden-xs-down proce">
+							<img src="rec/img/recetas/img1.png"  class="img-responsive">
+						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-lg-6 col-md-6">
-						<div class="icon-down">
+			</section>
+			<section class="mob-recetario">
+				<div class="cont-recetas">
+					<div class="row encabezado">
+						<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+							<div class="titulos">
+								Espagueti con pollo Pilgrim’s<br> en salsa de tomate
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+							<img id="rec-inco" src="rec/img/recetas/incomparable.png" class="img-responsive">
+						</div>
+					</div>
+						<div class="hoja">
 							<div class="row">
-								<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-center">
-									<img src="rec/img/reloj.png" class="img-responsive gallina">
-									<b>60 min. aproximados</b>
+								<div class="ingredientes col-lg-6 col-md-6 col-sm-12 col-xs-12">
+									Ingredientes
 								</div>
-								<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-center">
-									<img src="rec/img/porcion.png" class="img-responsive gallina">
-									<b>4 Porciones</b>
+							</div>
+							<div class="row">
+								<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+									<p>4 Mitades de <b><i>Pechuga de Pollo Pilgrim’s</i></b> sin hueso.</p><br>
+									<a href="milanesas.php" data-toggle="tooltip" data-placement="top" title="Ver producto">
+										<img id="producto" src="rec/img/recetas/pollo-fresco.png" class="img-responsive">
+									</a> 
+									<br>
+									<p>2 Cucharadas de aceite de oliva.</p>
+									<p>1/2 Cebolla picada.</p>
+									<p>1 Zanahoria picada.</p>
+									<p>1 Tallo de apio con hojas, lavado y sin fibra, picado.</p>
+									<p>2 Diente de ajo machacado.</p>
 								</div>
-								<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-center">
-									<img id="descargar" src="rec/img/down.png" class="img-responsive gallina">
-									<b>Descarga</b>
+								<div class="col-lg-6 col-md-6 hidden-xs-down">
+									<p>1/2 Taza de vino blanco seco.</p>
+									<p>1 Lata de puré de tomate.</p>
+									<p>1/2 Taza de caldo de pollo.</p>
+									<p>1 Cucharada de perejil lavado y picado.</p>
+									<p>3 Cucharadas de albahaca fresca, lavada y picada.</p>
+									<p>Sal y pimienta al gusto.</p>
+									<p>500 Gramos de espagueti.</p>
+								</div>
+								<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+									<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+										<img src="rec/img/recetas/reloj.png" class="img-responsive iconos">
+										<div class="indicadores">
+											<i><b>Tiempo de preparación: 60 minutos</b></i>
+										</div>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+										<img src="rec/img/recetas/rinde.png" class="img-responsive iconos">
+										<div class="indicadores">
+											<i><b>Rinde para : 4 personas</b></i>
+										</div>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+										<img src="rec/img/recetas/tipo.png" class="img-responsive iconos">
+										<div class="indicadores">
+											<i><b>Comida</b></i>
+										</div>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+										<img src="rec/img/recetas/reloj.png" class="img-responsive iconos">
+										<div class="indicadores">
+											<i><b>Descargar</b></i>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<!--<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="vertical-align: bottom;">
-						<video width="95%" controls id="video-corpo">
-							<source src="rec/video/nescafe-background.mp4" type="video/mp4">
-							<source src="mov_bbb.ogg" type="video/ogg">
-						</video>
-					</div>-->
 				</div>
-			</div>
+			</section>
 		</section>
 	</div>
 	<?php
